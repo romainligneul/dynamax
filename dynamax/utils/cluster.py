0f3a6c531be8f62b378495baf55d93011d854c04
gileshd <gileshd@googlemail.com>
2024-07-12 09:59:35 +0100

    Update kmeans_sklearn to also return labels

--- dynamax/utils/cluster.py
+++ dynamax/utils/cluster.py
@@ -1,13 +1,13 @@
-from typing import NamedTuple
+from typing import NamedTuple, Tuple
 
 from jax import Array, lax
 from jax import numpy as jnp
 from jax import random as jr
 
 
-def kmeans_centers_sklearn(k:int, X:Array, key:Array):
+def kmeans_sklearn(k:int, X:Array, key:Array) -> Tuple[Array, Array]:
     """
-    Compute the cluster centers using the K-means algorithm.
+    Compute the cluster centers and assignments using the sklearn K-means algorithm.
 
     Args:
         k (int): The number of clusters.
@@ -15,14 +15,14 @@ def kmeans_centers_sklearn(k:int, X:Array, key:Array):
         key (Array): The random seed array.
 
     Returns:
-        Array(k, D): The cluster centers.
+        Array(k, D), Array(N,): The cluster centers and labels
     """
     from sklearn.cluster import KMeans
 
     key, subkey = jr.split(key)  # Create a random seed for SKLearn.
     sklearn_key = jr.randint(subkey, shape=(), minval=0, maxval=2147483647)  # Max int32 value.
     km = KMeans(k, random_state=int(sklearn_key)).fit(X)
-    return jnp.array(km.cluster_centers_)
+    return jnp.array(km.cluster_centers_), jnp.array(km.labels_)
 
 
 class KMeansState(NamedTuple):
